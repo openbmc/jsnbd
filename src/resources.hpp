12d76c8e1cfc82af687260a93fea5516702c4d45
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2021-12-23 01:04:47 +0100

    Introduce state machine skeleton and events
    Virtual media service uses state machine built on:
    - state objects, holding state data and available transition patterns
    and,
    - events, allowing transitions between states.
    
    For each slot (defined in configuration file) independent state machine is
    created.
    
    Events structure is based on simple BasicEvent on which specific events
    like Mount, Unmount or RegisterDbusEvent are built.
    
    This commit just defines a skeleton for states - BasicState. Distinct
    states will be added in the next patches.
    
    Tested:
    Compiles and runs, UT passed:
    [==========] 17 tests from 4 test suites ran. (5 ms total)
    [  PASSED  ] 17 tests.
    
    Change-Id: I538beb20bae59112bb93ae7c5d2ba1817b7058af
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ src/resources.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "system.hpp"
+
+#include <sys/mount.h>
+
+#include <filesystem>
+#include <memory>
+#include <system_error>
+
+namespace interfaces
+{
+struct MountPointStateMachine;
+}
+
+namespace resource
+{
+
+class Error : public std::runtime_error
+{
+  public:
+    Error(std::errc errorCode, const std::string& message) :
+        std::runtime_error(message), errorCode(errorCode)
+    {}
+
+    const std::errc errorCode;
+};
+
+class Directory
+{
+  public:
+    Directory() = delete;
+    Directory(const Directory&) = delete;
+    Directory(Directory&& other) = delete;
+    Directory& operator=(const Directory&) = delete;
+    Directory& operator=(Directory&& other) = delete;
+
+    explicit Directory(const std::filesystem::path& name) :
+        path(std::filesystem::temp_directory_path() / name)
+    {
+        std::error_code ec;
+
+        if (!std::filesystem::create_directory(path, ec))
+        {
+            LogMsg(Logger::Error, ec,
+                   " : Unable to create mount directory: ", path);
+            throw Error(std::errc::io_error,
+                        "Failed to create mount directory");
+        }
+    }
+
+    ~Directory()
+    {
+        std::error_code ec;
+
+        if (!std::filesystem::remove(path, ec))
+        {
+            LogMsg(Logger::Error, ec, " : Unable to remove directory ", path);
+        }
+    }
+
+    std::filesystem::path getPath() const
+    {
+        return path;
+    }
+
+  private:
+    std::filesystem::path path;
+};
+
+class Mount
+{
+  public:
+    Mount() = delete;
+    Mount(const Mount&) = delete;
+    Mount(Mount&& other) = delete;
+    Mount& operator=(const Mount&) = delete;
+    Mount& operator=(Mount&& other) = delete;
+
+    explicit Mount(std::unique_ptr<Directory> directory) :
+        directory(std::move(directory))
+    {}
+
+    ~Mount() = default;
+
+    std::filesystem::path getPath() const
+    {
+        return directory->getPath();
+    }
+
+  private:
+    std::unique_ptr<Directory> directory;
+};
+
+} // namespace resource
