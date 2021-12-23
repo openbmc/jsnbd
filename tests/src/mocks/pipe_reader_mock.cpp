99d409637dc9a295b1606216764bfc7d3a813b7b
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2021-12-23 01:24:07 +0100

    Add udev support and device monitor
    Added coroutine (DeviceMonitor) to detect changes of size of NBD
    devices, calling associated callbacks.
    
    Those callbacks are the start points of future transitions between
    states (configuration and deconfiguration of underlying nbd device
    backends)
    
    Process object has been added used to execute backends capturing
    output and exit code.
    
    Tested:
    Manual/external changes in observed ndb devices triggers actions.
    UT Passed:
    [==========] 33 tests from 7 test suites ran. (4237 ms total)
    [  PASSED  ] 33 tests.
    
    Change-Id: I9bbb502db355c39d9764f2f483b982461b367a03
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ tests/src/mocks/pipe_reader_mock.cpp
@@ -0,0 +1,25 @@
+#include "utils/pipe_reader.hpp"
+
+#include <boost/asio/async_result.hpp>
+#include <boost/asio/error.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <type_traits>
+
+namespace utils
+{
+
+PipeReader::PipeReader(boost::asio::io_context& ioc) : pipe(ioc)
+{}
+
+size_t
+    PipeReader::asyncRead([[maybe_unused]] Buffer&& buf,
+                          [[maybe_unused]] boost::asio::yield_context&& yield,
+                          boost::system::error_code& error)
+{
+    error = boost::asio::error::misc_errors::eof;
+
+    return 0;
+}
+
+} // namespace utils
---
+++ tests/src/mocks/udev_mock.cpp
@@ -0,0 +1,60 @@
+#include "mocks/udev_mock.hpp"
+
+#include "utils/udev.hpp"
+
+MockUdevDeviceEngine* MockUdevDevice::engine = nullptr;
+
+namespace utils
+{
+
+Udev::Udev()
+{}
+
+Udev::~Udev()
+{}
+
+UdevMonitor::UdevMonitor([[maybe_unused]] const Udev& udev,
+                         [[maybe_unused]] const std::string& name)
+{}
+
+UdevMonitor::~UdevMonitor()
+{}
+
+int UdevMonitor::addFilter([[maybe_unused]] const std::string& subsystem,
+                           [[maybe_unused]] const std::string& devType)
+{
+    return 0;
+}
+
+int UdevMonitor::enable()
+{
+    return 0;
+}
+
+int UdevMonitor::getFd()
+{
+    return 0;
+}
+
+UdevDevice::UdevDevice([[maybe_unused]] const UdevMonitor& monitor)
+{}
+
+UdevDevice::~UdevDevice()
+{}
+
+std::string UdevDevice::getAction()
+{
+    return MockUdevDevice::getAction();
+}
+
+std::string UdevDevice::getSysname()
+{
+    return MockUdevDevice::getSysname();
+}
+
+std::string UdevDevice::getSysAttrValue(const std::string& sysAttr)
+{
+    return MockUdevDevice::getSysAttrValue(sysAttr);
+}
+
+} // namespace utils
