416a78090cd263880b21d92070a6cd58e0d5e32c
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-01-03 18:26:42 +0100

    Add Legacy mode CIFS support
    Mount flow:
    -  dbus "Mount" (emitMountEvent) initiates two actions
      - mount cifs share using ::mount()
        - using most secure v3.1 protocol,
        - if not available use v3.0 as fallback
      - start nbdkit with file plugin on image/iso file mounted locally
        - nbdkit spawns nbd-client internally
      this start ReadyState --> ActivatingState transition
    - once nbd device is set up, udev event will change
    state (emitUDevStateChangeEvent triggers ActivatingState --> ActiveState
    transition.
    - dbus signal is sent on ActiveState entry to notify bmcweb on
    completion.
    
    Umnount flow:
    - nbddir and nbd-client are closed as result of
      1) connection from server closed
      2) dbus "Unmount" stops nbd processes
      this triggers emitUnmountEvent and emitSubProcessStoppedEvent
      (ActiveState --> DeactivatingState)
    - once nbd device is closed/deactivated udev triggers
    StateChange::removed. When both subprocess is closed and device is
    removed DeactivatingState --> ReadyState transition is invoked.
    
    Tested:
    manual inserting:
    POST ...InsertMedia { "Image": "smb://<hostname>/<share>/<file>", "Username": "<username>", "Password":"<password>" }
    manual ejecting:
    POST ...EjectMedia {}
    
    UT passed:
    [==========] 110 tests from 22 test suites ran. (6913 ms total)
    [  PASSED  ] 110 tests.
    
    Change-Id: Ia66224c81a88718014d4620f60b7ae460e9763bc
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ tests/src/mocks/mounter_mock.cpp
@@ -0,0 +1,48 @@
+#include "logger.hpp"
+#include "mocks/mounter_test_dirs.hpp"
+#include "utils/mounter.hpp"
+
+#include <system_error>
+
+namespace utils
+{
+
+int result = -1;
+
+Mounter::Mounter([[maybe_unused]] const char* source, const char* target,
+                 [[maybe_unused]] const char* filesystemtype,
+                 [[maybe_unused]] unsigned long mountflags, const void* data) :
+    target(target)
+{
+    std::cout << "MOCK MOUNTER MOCK MOUNTER MOCK MOUNTER MOCK MOUNTER MOCK "
+                 "MOUNTER MOCK MOUNTER"
+              << std::endl;
+    std::string mountDir(target);
+    std::string options(static_cast<const char*>(data));
+
+    if ((target == directories::mountDir1) &&
+        (options.find("vers=3.1.1") != std::string::npos))
+    {
+        throw std::system_error(std::make_error_code(std::errc::bad_address));
+    }
+
+    if (target == directories::mountDir2)
+    {
+        throw std::system_error(
+            std::make_error_code(std::errc::no_such_file_or_directory));
+    }
+}
+
+Mounter::~Mounter()
+{
+    std::string mountDir(target);
+    result = 0;
+    if (target == directories::mountDir3)
+    {
+        result = 1;
+        LogMsg(Logger::Error, "Unable to unmout directory ", target,
+               ", ec=", EBUSY);
+    }
+}
+
+} // namespace utils
