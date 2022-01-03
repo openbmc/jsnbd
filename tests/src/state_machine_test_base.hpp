5bf16fda0d6d528b70b882ee1fa2f634b17b9e2b
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-01-03 18:24:10 +0100

    Implemented proxy mode support
    Proxy mode flow + notification mechanism
    
    Mount flow:
    - spawning nbd-client as a reaction on dbus "Mount" (emitMountEvent
    initializes ReadyState --> ActivatingState transition
    - once nbd device is set up, udev event will change
    state (emitUDevStateChangeEvent triggers ActivatingState --> ActiveState
    transition.
    - dbus signal is sent on ActiveState entry to notify bmcweb on
    completion.
    
    Umnount flow:
    - nbd-client closed as result of
      1) connection from server closed
      2) dbus "Unmount" stops nbd-client process
      what triggers emitUnmountEvent and emitSubProcessStoppedEvent
      (ActiveState --> DeactivatingState)
    - once nbd device is closed/deactivated udev triggers
    StateChange::removed. When both subprocess is closed and device is
    removed DeactivatingState --> ReadyState transition is invoked.
    
    Tested:
    On external trigger, nbd-client starts and connection between
    nbd server and usbgadget is established.
    UT passed:
    [==========] 94 tests from 18 test suites ran. (6667 ms total)
    [  PASSED  ] 94 tests.
    
    Change-Id: Ifae276ca9bbab08da7a1b471db52b0ff545d40b3
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

--- tests/src/state_machine_test_base.hpp
+++ tests/src/state_machine_test_base.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include "mocks/file_printer_mock.hpp"
 #include "mocks/utils_mock.hpp"
 #include "state_machine.hpp"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+using MockWrapper = testing::NiceMock<utils::MockNotificationWrapper>;
 
 struct TestableMountPointStateMachine : public MountPointStateMachine
 {
@@ -15,14 +18,31 @@ struct TestableMountPointStateMachine : public MountPointStateMachine
         MountPointStateMachine(ioc, devMonitor, name, config)
     {
         bus = std::make_shared<sdbusplus::asio::connection>(ioc);
+        wrapper = std::make_unique<MockWrapper>();
+    }
+
+    void notificationInitialize(
+        [[maybe_unused]] std::shared_ptr<sdbusplus::asio::connection> con,
+        [[maybe_unused]] const std::string& svc,
+        [[maybe_unused]] const std::string& iface,
+        [[maybe_unused]] const std::string& name) override
+    {
+        completionNotification = std::move(wrapper);
     }
 
     std::shared_ptr<sdbusplus::asio::connection> bus;
+    std::unique_ptr<MockWrapper> wrapper;
 };
 
 class StateMachineTestBase
 {
   protected:
+    StateMachineTestBase()
+    {
+        MockFilePrinter::engine = &printer;
+    }
+
+    testing::NiceMock<MockFilePrinterEngine> printer;
     boost::asio::io_context ioc;
     NBDDevice<> dev{"nbd0"};
     DeviceMonitor monitor{ioc};
