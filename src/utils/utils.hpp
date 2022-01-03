cae137d9543c7f2c6f0c451fe6ab7f3e8bc5f106
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-01-03 18:26:42 +0100

    Add Legacy mode CIFS support
    Mount flow:
    -  dbus "Mount" (emitMountEvent) initiates two actions
      - mount cifs share using ::mount()
        - using most secure v3.1 protocol,
        - if not available use v3.0 as fallback
      - start nbdkit with file plugin on image/iso file mounted locally
        - nbdkit spawns nbd-client internally
      this start ReadyState --> ActivatingState transition
    - once nbd device is set up, udev event will change
    state (emitUDevStateChangeEvent triggers ActivatingState --> ActiveState
    transition.
    - dbus signal is sent on ActiveState entry to notify bmcweb on
    completion.
    
    Unmount flow:
    - nbddir and nbd-client are closed as result of
      1) connection from server closed
      2) dbus "Unmount" stops nbd processes
      this triggers emitUnmountEvent and emitSubProcessStoppedEvent
      (ActiveState --> DeactivatingState)
    - once nbd device is closed/deactivated udev triggers
    StateChange::removed. When both subprocess is closed and device is
    removed DeactivatingState --> ReadyState transition is invoked.
    
    Tested:
    manual inserting:
    POST ...InsertMedia { "Image": "smb://<hostname>/<share>/<file>", \
    Username": "<username>", "Password":"<password>" }
    manual ejecting:
    POST ...EjectMedia {}
    
    UT passed:
    [==========] 110 tests from 22 test suites ran. (6913 ms total)
    [  PASSED  ] 110 tests.
    
    Change-Id: Ia66224c81a88718014d4620f60b7ae460e9763bc
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
    Signed-off-by: Jakub Kalinowski <jakubx.kalinowski@intel.com>
    Signed-off-by: Wojciech Tempczyk <wojciechx.tempczyk@intel.com>
    Signed-off-by: Boleslaw Ogonczyk Makowski <boleslawx.ogonczyk-makowski@intel.com>

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -5,15 +5,146 @@
 #include <unistd.h>
 
 #include <boost/system/error_code.hpp>
+#include <sdbusplus/exception.hpp>
 
+#include <algorithm>
 #include <chrono>
+#include <cstring>
 #include <filesystem>
+#include <fstream>
 #include <functional>
 #include <optional>
+#include <string>
 #include <system_error>
+#include <type_traits>
+#include <vector>
 
 namespace utils
 {
+
+constexpr const size_t secretLimit = 1024;
+
+template <typename T>
+static void secureCleanup(T& value)
+{
+    auto raw = const_cast<typename T::value_type*>(&value[0]);
+    explicit_bzero(raw, value.size() * sizeof(*raw));
+}
+
+class Credentials
+{
+  public:
+    Credentials(std::string&& user, std::string&& password) :
+        userBuf(std::move(user)), passBuf(std::move(password))
+    {}
+    Credentials() = delete;
+    Credentials(const Credentials&) = delete;
+    Credentials& operator=(const Credentials&) = delete;
+
+    ~Credentials()
+    {
+        secureCleanup(userBuf);
+        secureCleanup(passBuf);
+    }
+
+    const std::string& user()
+    {
+        return userBuf;
+    }
+
+    const std::string& password()
+    {
+        return passBuf;
+    }
+
+    void escapeCommas()
+    {
+        if (!commasEscaped)
+        {
+            escapeComma(passBuf);
+            commasEscaped = true;
+        }
+    }
+
+  private:
+    /* escape ',' (coma) by ',,' */
+    void escapeComma(std::string& s)
+    {
+        std::string temp;
+        std::for_each(s.begin(), s.end(), [&temp](const auto& c) {
+            *std::back_inserter(temp) = c;
+            if (c == ',')
+            {
+                *std::back_inserter(temp) = c;
+            }
+        });
+        std::swap(s, temp);
+        secureCleanup(temp);
+    }
+
+    std::string userBuf;
+    std::string passBuf;
+    bool commasEscaped{false};
+};
+
+class CredentialsProvider
+{
+  public:
+    template <typename T>
+    struct Deleter
+    {
+        void operator()(T* buff) const
+        {
+            if (buff)
+            {
+                secureCleanup(*buff);
+                delete buff;
+            }
+        }
+    };
+
+    using Buffer = std::vector<char>;
+    using SecureBuffer = std::unique_ptr<Buffer, Deleter<Buffer>>;
+    // Using explicit definition instead of std::function to avoid implicit
+    // conversions eg. stack copy instead of reference for parameters
+    using FormatterFunc = void(const std::string& username,
+                               const std::string& password, Buffer& dest);
+
+    CredentialsProvider(std::string&& user, std::string&& password) :
+        credentials(std::move(user), std::move(password))
+    {}
+
+    void escapeCommas()
+    {
+        credentials.escapeCommas();
+    }
+
+    const std::string& user()
+    {
+        return credentials.user();
+    }
+
+    const std::string& password()
+    {
+        return credentials.password();
+    }
+
+    SecureBuffer pack(FormatterFunc formatter)
+    {
+        if (!formatter)
+        {
+            return nullptr;
+        }
+
+        SecureBuffer packed{new Buffer{}};
+        formatter(credentials.user(), credentials.password(), *packed);
+
+        return packed;
+    }
+
+  private:
+    Credentials credentials;
+};
 
 class FileObject
 {
@@ -52,6 +183,69 @@ class FileObject
 
   private:
     int fd;
+};
+
+template <class File = FileObject>
+class VolatileFile
+{
+    using Buffer = CredentialsProvider::SecureBuffer;
+
+  public:
+    VolatileFile(Buffer&& contents) : size(contents->size())
+    {
+        auto data = std::move(contents);
+        filePath = std::filesystem::temp_directory_path().c_str();
+        filePath.append("/VM-XXXXXX");
+        create(data);
+    }
+
+    ~VolatileFile()
+    {
+        purgeFileContents();
+        std::filesystem::remove(filePath);
+    }
+
+    const std::string& path()
+    {
+        return filePath;
+    }
+
+  protected:
+    void create(const Buffer& data)
+    {
+        auto fd = mkstemp(filePath.data());
+
+        File file(fd);
+        if (file.write(data->data(), static_cast<ssize_t>(data->size())) !=
+            static_cast<ssize_t>(data->size()))
+        {
+            throw sdbusplus::exception::SdBusError(
+                EIO, "I/O error on temporary file");
+        }
+    }
+
+    void purgeFileContents()
+    {
+        if (std::ofstream file(filePath); file)
+        {
+            std::array<char, secretLimit> buf;
+            buf.fill('*');
+
+            std::size_t bytesWritten = 0;
+            while (bytesWritten < size)
+            {
+                std::size_t bytesToWrite = std::min(secretLimit,
+                                                    (size - bytesWritten));
+                file.write(buf.data(),
+                           static_cast<std::streamsize>(bytesToWrite));
+                bytesWritten += bytesToWrite;
+            }
+        }
+    }
+
+  private:
+    std::string filePath;
+    const std::size_t size;
 };
 
 class SignalSender
