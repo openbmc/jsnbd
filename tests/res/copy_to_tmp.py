e1c60bb428d7e842db3c06563b6e0f2aa664dbaa
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-07-14 16:04:08 +0200

    Replace old base - new service added
    This commit adds all build infrastructure and main with some basic
    stuff.
    
    New service infrastructure is as follows:
    - Meson build system
    - virtual-media/config.json config file
    - xyz.openbmc_project.VirtualMedia.service
    - tmpfiles.d/virtual-media.conf configuration for
    systemd-tmpfiles-clean.service for VM temporary files
    - start code base
      - logging facility
      - configuration management
      - NBD device management
    - UT added
      - added tests/ tree
      - configuration UTs
      - NBD device UTs
    
    Tested:
    At this moment it compiles and reads the configuration.
    UT passed:
    [==========] 15 tests from 3 test suites ran. (3 ms total)
    [  PASSED  ] 15 tests.
    
    Change-Id: Ic4578c5863dc196628cdcfa9e453803c4d749751
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ tests/res/copy_to_tmp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+input_path = os.path.join(
+    os.getenv('MESON_SOURCE_ROOT'),
+    os.getenv('MESON_SUBDIR'),
+    sys.argv[1])
+
+output_path = os.path.join('/tmp', sys.argv[2])
+
+shutil.copyfile(input_path, output_path)
