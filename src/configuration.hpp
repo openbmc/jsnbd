bab06e1a2b56adda1c974316ce90a5b078d7f4f3
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-07-14 16:04:08 +0200

    Replace old base - new service added
    This commit adds all build infrastructure and main application skeleton.
    
    New service infrastructure is as follows:
    - Meson build system
    - virtual-media/config.json config file
    - xyz.openbmc_project.VirtualMedia.service
    - start code base
      - logging facility
      - configuration management
      - NBD device management
    - UT added
      - added tests/ tree
      - configuration UTs
      - NBD device UTs
    
    Tested:
    Service is successfully installed on target and runs on startup without
    any exceptions (which means config file was parsed correctly). At this
    state there's nothing more to test on target, however this will change
    with further patches, each introducing some core functionality to test.
    
    UT passed:
    [==========] 25 tests from 4 test suites ran. (39 ms total)
    [  PASSED  ] 25 tests.
    
    Change-Id: Ic4578c5863dc196628cdcfa9e453803c4d749751
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
    Signed-off-by: Jakub Kalinowski <jakubx.kalinowski@intel.com>
    Signed-off-by: Eryk Siejk <erykx.siejk@intel.com>
    Signed-off-by: Ed Tanous <ed@tanous.net>

---
+++ src/configuration.hpp
@@ -0,0 +1,193 @@
+#pragma once
+
+#include "system.hpp"
+#include "utils/log-wrapper.hpp"
+#include "utils/utils.hpp"
+
+#include <boost/beast/core/file_base.hpp>
+#include <nlohmann/json.hpp>
+
+#include <cstdint>
+#include <exception>
+#include <filesystem>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+class Configuration
+{
+  public:
+    enum class Mode
+    {
+        // Proxy mode - works directly from browser and uses JavaScript/HTML5
+        // to communicate over Secure WebSockets directly to HTTPS endpoint
+        // hosted by bmcweb on BMC
+        proxy = 0,
+
+        // Standard mode - is initiated from browser using Redfish defined
+        // VirtualMedia schemas, then BMC process connects to external
+        // CIFS/HTTPS image pointed during initialization.
+        standard = 1,
+
+        invalid
+    };
+
+    struct MountPoint
+    {
+        static constexpr uint64_t timeout = 30;
+
+        NBDDevice<> nbdDevice;
+        std::string unixSocket;
+        std::string endPointId;
+        Mode mode{Mode::proxy};
+
+        static std::vector<std::string> toArgs(const MountPoint& mp)
+        {
+            const auto timeout =
+                std::to_string(Configuration::MountPoint::timeout);
+            return {"-t", timeout, "-u", mp.unixSocket, mp.nbdDevice.toPath(),
+                    "-nL"};
+        }
+    };
+
+    std::optional<const MountPoint*>
+        getMountPoint(const std::string& name) const
+    {
+        const auto mp = mountPoints.find(name);
+        if (mp != mountPoints.end())
+        {
+            return &mp->second;
+        }
+
+        return std::nullopt;
+    }
+
+    std::unordered_map<std::string, MountPoint> mountPoints;
+    bool valid{false};
+
+    explicit Configuration(const std::string& file) :
+        valid(loadConfiguration(file))
+    {}
+
+  private:
+    bool loadConfiguration(const std::string& file) noexcept
+    {
+        try
+        {
+            utils::FileObject configFile(file, boost::beast::file_mode::read);
+            std::optional<std::string> configData = configFile.readContent();
+            if (!configData)
+            {
+                LOGGER_ERROR("Couldn't read contents of '{}'", file);
+                return false;
+            }
+
+            nlohmann::json data = nlohmann::json::parse(*configData, nullptr,
+                                                        false);
+            if (data.is_discarded())
+            {
+                LOGGER_ERROR("Invalid configuration data");
+                return false;
+            }
+
+            return setupVariables(data);
+        }
+        catch (const std::filesystem::filesystem_error& e)
+        {
+            LOGGER_CRITICAL("Could not open configuration file '{}': {}", file,
+                            e.what());
+            return false;
+        }
+        catch (const std::exception& e)
+        {
+            LOGGER_CRITICAL("Could not set up configuration: {}", e.what());
+            return false;
+        }
+    }
+
+    bool setupVariables(const nlohmann::json& config)
+    {
+        size_t proxyMountPoints = 0;
+        size_t standardMountPoints = 0;
+        for (const auto& item : config.items())
+        {
+            if (item.key() == "ProxyMountPoints")
+            {
+                proxyMountPoints = item.value();
+            }
+            if (item.key() == "StandardMountPoints")
+            {
+                standardMountPoints = item.value();
+            }
+        }
+
+        return createMountPointsMap(proxyMountPoints, standardMountPoints);
+    }
+
+    bool createMountPointsMap(const size_t proxyMountPoints,
+                              const size_t standardMountPoints)
+    {
+        constexpr std::string_view slotPrefix = "Slot";
+        size_t mountPointCounter = 0;
+
+        for (size_t i = 0; i < proxyMountPoints; i++)
+        {
+            std::optional<MountPoint> mp = createMountPoint(mountPointCounter,
+                                                            Mode::proxy);
+            if (!mp)
+            {
+                return false;
+            }
+            std::string slotStr = std::format("{}_{}", slotPrefix,
+                                              mountPointCounter);
+            mountPoints[slotStr] = std::move(*mp);
+            mountPointCounter++;
+        }
+
+        for (size_t i = 0; i < standardMountPoints; i++)
+        {
+            std::optional<MountPoint> mp = createMountPoint(mountPointCounter,
+                                                            Mode::standard);
+            if (!mp)
+            {
+                return false;
+            }
+            std::string slotStr = std::format("{}_{}", slotPrefix,
+                                              mountPointCounter);
+            mountPoints[slotStr] = std::move(*mp);
+            mountPointCounter++;
+        }
+
+        return true;
+    }
+
+    static std::optional<MountPoint> createMountPoint(const size_t index,
+                                                      const Mode mode)
+    {
+        MountPoint mp;
+        std::string nbdDevId = std::format("nbd{}", index);
+        mp.nbdDevice = NBDDevice(nbdDevId);
+
+        if (!mp.nbdDevice.isValid())
+        {
+            return {};
+        }
+
+        std::string unixSocketStr = std::format("/run/virtual-media/nbd{}.sock",
+                                                index);
+        mp.unixSocket = unixSocketStr;
+
+        mp.mode = mode;
+
+        if (mode == Mode::proxy)
+        {
+            std::string endPointIdStr = std::format("/nbd/{}", index);
+            mp.endPointId = endPointIdStr;
+        }
+
+        return mp;
+    }
+};
