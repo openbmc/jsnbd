1778179a5a49e877524de885cbc9a42e09d160b6
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-07-14 16:04:08 +0200

    Replace old base - new service added
    This commit adds all build infrastructure and main with some basic
    stuff.
    
    New service infrastructure is as follows:
    - Meson build system
    - virtual-media/config.json config file
    - xyz.openbmc_project.VirtualMedia.service
    - tmpfiles.d/virtual-media.conf configuration for
    systemd-tmpfiles-clean.service for VM temporary files
    - start code base
      - logging facility
      - configuration management
      - NBD device management
    - UT added
      - added tests/ tree
      - configuration UTs
      - NBD device UTs
    
    Linting is temporarily disabled
    
    Tested:
    At this moment it compiles and reads the configuration.
    UT passed:
    [==========] 15 tests from 3 test suites ran. (3 ms total)
    [  PASSED  ] 15 tests.
    
    Change-Id: Ic4578c5863dc196628cdcfa9e453803c4d749751
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include "configuration.hpp"
+
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <filesystem>
+#include <memory>
+#include <system_error>
+
+std::chrono::seconds Configuration::inactivityTimeout;
+
+class App
+{
+  public:
+    App(boost::asio::io_context& ioc)
+    {
+        auto bus = std::make_shared<sdbusplus::asio::connection>(ioc);
+        objServer = std::make_shared<sdbusplus::asio::object_server>(bus);
+        bus->request_name("xyz.openbmc_project.VirtualMedia");
+        objManager = std::make_shared<sdbusplus::server::manager_t>(
+            *bus, "/xyz/openbmc_project/VirtualMedia");
+    }
+
+  private:
+    std::shared_ptr<sdbusplus::asio::object_server> objServer;
+    std::shared_ptr<sdbusplus::server::manager_t> objManager;
+};
+
+int main()
+{
+    Configuration config("/usr/share/virtual-media/config.json");
+    if (!config.valid)
+    {
+        return -1;
+    }
+
+    // setup secure ownership for newly created files (always succeeds)
+    umask(Configuration::defaultUmask);
+
+    boost::asio::io_context ioc;
+    boost::asio::signal_set signals(ioc, SIGINT, SIGTERM);
+    signals.async_wait(
+        [&ioc](const boost::system::error_code&, const int&) { ioc.stop(); });
+
+    App app(ioc);
+
+    ioc.run();
+
+    return 0;
+}
