28b427a4de4325f1fff2995ddc230001cd673942
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-07-14 16:04:08 +0200

    Replace old base - new service added
    This commit adds all build infrastructure and main with some basic
    stuff.
    
    New service infrastructure is as follows:
    - Meson build system
    - virtual-media/config.json config file
    - xyz.openbmc_project.VirtualMedia.service
    - tmpfiles.d/virtual-media.conf configuration for
    systemd-tmpfiles-clean.service for VM temporary files
    - start code base
      - logging facility
      - configuration management
      - NBD device management
    - UT added
      - added tests/ tree
      - configuration UTs
      - NBD device UTs
    
    Tested:
    At this moment it compiles and reads the configuration.
    UT passed:
    [==========] 15 tests from 3 test suites ran. (3 ms total)
    [  PASSED  ] 15 tests.
    
    Change-Id: Ic4578c5863dc196628cdcfa9e453803c4d749751
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ tests/src/configuration_test.cpp
@@ -0,0 +1,103 @@
+#include "configuration.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+chrono::seconds Configuration::inactivityTimeout;
+
+TEST(ConfigurationLoadingTest, CfgInvalidForNotExistingFile)
+{
+    Configuration nonExistent{"non-existent.json"};
+    EXPECT_EQ(nonExistent.valid, false);
+}
+
+class ConfigurationTest : public ::testing::Test
+{
+  protected:
+    const Configuration::MountPoint* mp;
+    const std::string virtual_media_json_path = "virtual-media.json";
+    const std::string virtual_media_json_path_nbd15 =
+        "virtual-media-nbd15.json";
+    const std::string virtual_media_json_path_nbd16 =
+        "virtual-media-nbd16.json";
+    const std::string virtual_media_json_invalid_path =
+        "virtual-media-invalid.json";
+    const std::string virtual_media_json_missing_nbd =
+        "virtual-media-missing-nbd.json";
+    const char* slot_list[4] = {"Slot_0", "Slot_1", "Slot_2", "Slot_3"};
+    Configuration config{constructPath(virtual_media_json_path)};
+
+  public:
+    inline std::string constructPath(const std::string& file)
+    {
+        return "/tmp/" + file;
+    }
+
+    void corruptJsonFile(const std::string& filePath)
+    {
+        ofstream file;
+        file.open(filePath, ofstream::out | ofstream::trunc);
+        file << "corruption";
+        file.close();
+    }
+};
+
+TEST_F(ConfigurationTest, GetMountPointsSuccessfully)
+{
+    ASSERT_EQ(config.valid, true);
+    for (int i = 0; i < 4; i++)
+    {
+        ASSERT_EQ(config.getMountPoint(slot_list[i]),
+                  &config.mountPoints[slot_list[i]]);
+    }
+}
+
+TEST_F(ConfigurationTest, GetMountPointNBDDeviceSuccessfully)
+{
+    Configuration nbd15{constructPath(virtual_media_json_path_nbd15)};
+    mp = nbd15.getMountPoint(slot_list[0]).value();
+    ASSERT_EQ(mp->nbdDevice.toString(),
+              (&nbd15.mountPoints[slot_list[0]])->nbdDevice.toString());
+}
+
+TEST_F(ConfigurationTest, GetMountPointNBDDeviceUnsuccessfully)
+{
+    Configuration nbd16{constructPath(virtual_media_json_path_nbd16)};
+    ASSERT_EQ(nbd16.getMountPoint(slot_list[0]), nullopt);
+}
+
+TEST_F(ConfigurationTest, GetMountPointNBDDeviceMissing)
+{
+    Configuration missingNbd{constructPath(virtual_media_json_missing_nbd)};
+    ASSERT_EQ(missingNbd.getMountPoint(slot_list[0]), nullopt);
+}
+
+TEST_F(ConfigurationTest, CfgLoadedSuccessfully)
+{
+    ASSERT_EQ(config.valid, true);
+}
+
+TEST_F(ConfigurationTest, CfgInvalidForInvalidJsonFile)
+{
+    std::string invalidPath = constructPath(virtual_media_json_invalid_path);
+    corruptJsonFile(invalidPath);
+    Configuration notValidJson{invalidPath};
+
+    EXPECT_EQ(notValidJson.valid, false);
+    filesystem::remove(invalidPath);
+}
+
+TEST_F(ConfigurationTest, GetMountPointInvalidForNotExistingPoint)
+{
+    EXPECT_EQ(config.getMountPoint("non-existing"), nullopt);
+}
