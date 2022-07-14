7f97f58f175df792aba156ccc7e3bc91ecb01073
Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>
2022-07-14 16:04:08 +0200

    Replace old base - new service added
    This commit adds all build infrastructure and main with some basic
    stuff.
    
    New service infrastructure is as follows:
    - Meson build system
    - virtual-media/config.json config file
    - xyz.openbmc_project.VirtualMedia.service
    - tmpfiles.d/virtual-media.conf configuration for
    systemd-tmpfiles-clean.service for VM temporary files
    - start code base
      - logging facility
      - configuration management
      - NBD device management
    - UT added
      - added tests/ tree
      - configuration UTs
      - NBD device UTs
    
    Tested:
    At this moment it compiles and reads the configuration.
    UT passed:
    [==========] 15 tests from 3 test suites ran. (3 ms total)
    [  PASSED  ] 15 tests.
    
    Change-Id: Ic4578c5863dc196628cdcfa9e453803c4d749751
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ src/system.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "logger.hpp"
+#include "utils/utils.hpp"
+
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <algorithm>
+#include <array>
+#include <compare>
+#include <filesystem>
+#include <map>
+#include <string>
+
+#define NBD_DISCONNECT _IO(0xab, 8)
+#define NBD_CLEAR_SOCK _IO(0xab, 4)
+
+template <class File = utils::FileObject>
+class NBDDevice
+{
+  public:
+    NBDDevice() = default;
+    explicit NBDDevice(const std::string_view& nbdName)
+    {
+        if (!nbdName.empty())
+        {
+            const auto iter = std::ranges::find(
+                std::cbegin(nameMatching), std::cend(nameMatching), nbdName);
+            if (iter != std::cend(nameMatching))
+            {
+                value = *iter;
+            }
+        }
+    }
+    NBDDevice(const NBDDevice&) = default;
+    NBDDevice(NBDDevice&&) noexcept = default;
+
+    NBDDevice& operator=(const NBDDevice&) = default;
+    NBDDevice& operator=(NBDDevice&&) noexcept = default;
+
+    auto operator<=>(const NBDDevice& rhs) const = default;
+
+    bool isValid() const
+    {
+        return (!value.empty());
+    }
+
+    explicit operator bool()
+    {
+        return isValid();
+    }
+
+    bool isReady() const
+    {
+        if (value.empty())
+        {
+            return false;
+        }
+
+        try
+        {
+            File file(toPath().c_str(), O_EXCL);
+        }
+        catch (std::filesystem::filesystem_error& e)
+        {
+            LogMsg(Logger::Error, "Ready check error: ", e.what());
+            return false;
+        }
+
+        return true;
+    }
+
+    bool disconnect() const
+    {
+        if (value.empty())
+        {
+            return false;
+        }
+
+        try
+        {
+            File file(toPath().c_str(), O_RDWR);
+            if (file.ioctl(NBD_DISCONNECT) < 0)
+            {
+                LogMsg(Logger::Info, "Ioctl failed: \n");
+            }
+            if (file.ioctl(NBD_CLEAR_SOCK) < 0)
+            {
+                LogMsg(Logger::Info, "Ioctl failed: \n");
+            }
+        }
+        catch (std::filesystem::filesystem_error& e)
+        {
+            LogMsg(Logger::Error, "Disconnect error: ", e.what());
+            return false;
+        }
+
+        return true;
+    }
+
+    const std::string& toString() const
+    {
+        return value;
+    }
+
+    std::filesystem::path toPath() const
+    {
+        if (value.empty())
+        {
+            return {};
+        }
+        return std::filesystem::path("/dev") / std::filesystem::path(value);
+    }
+
+  private:
+    std::string value{};
+
+    static constexpr std::array<std::string_view, 16> nameMatching = {
+        "nbd0", "nbd1", "nbd2",  "nbd3",  "nbd4",  "nbd5",  "nbd6",  "nbd7",
+        "nbd8", "nbd9", "nbd10", "nbd11", "nbd12", "nbd13", "nbd14", "nbd15"};
+};
