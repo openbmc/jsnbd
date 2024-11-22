84295ea5a27cf8f4bc22c28a045def02d47e909b
Michal Orzel <michalx.orzel@intel.com>
2024-11-22 12:04:25 +0100

    Add logger module
    Logger implementation is based on the one from bmcweb, however it
    utilizes phosphor-logging instead of write to stdout.
    
    Tested:
    - Local CI build passed
    - Test logs are present on BMC
    
    Change-Id: Iac65fa2631fb3f2da63dee3eb2ccc684c072f241
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>

--- virtual-media/src/main.cpp
+++ virtual-media/src/main.cpp
@@ -1,3 +1,5 @@
+#include "utils/log-wrapper.hpp"
+
 #include <boost/asio/error.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/signal_set.hpp>
@@ -8,7 +10,6 @@
 
 #include <csignal>
 #include <exception>
-#include <iostream>
 #include <memory>
 
 class App
@@ -40,9 +41,7 @@ int main()
             [&ioc](const boost::system::error_code& ec, const int signal) {
                 if (ec)
                 {
-                    std::cerr
-                        << "Error while waiting for signals: " << ec.what()
-                        << std::endl;
+                    LOGGER_ERROR("Error while waiting for signals: {}", ec);
                     if (ec == boost::asio::error::operation_aborted)
                     {
                         ioc.stop();
@@ -51,7 +50,7 @@ int main()
                     return;
                 }
 
-                std::cout << "Received signal " << signal << std::endl;
+                LOGGER_INFO("Received signal {}", signal);
                 ioc.stop();
             });
 
@@ -61,12 +60,12 @@ int main()
     }
     catch (const std::exception& e)
     {
-        std::cerr << "Error thrown to main: " << e.what() << std::endl;
+        LOGGER_CRITICAL("Error thrown to main: {}", e.what());
         return -1;
     }
     catch (...)
     {
-        std::cerr << "Error thrown to main." << std::endl;
+        LOGGER_CRITICAL("Error thrown to main.");
         return -1;
     }
 }
