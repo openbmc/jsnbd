8029bebe2d220e15f06772e4eb6c7a8ae937d7e8
Michal Orzel <michalx.orzel@intel.com>
2024-11-25 12:49:48 +0100

    Introduce state machine skeleton and events
    Virtual media service uses state machine built on:
    - state objects, holding state data and available transition patterns
    and,
    - events, allowing transitions between states.
    
    For each slot (defined in configuration file) independent state machine
    is created.
    
    Events structure is based on simple BasicEvent on which specific events
    like Mount, Unmount or RegisterDbusEvent are built.
    
    This commit just defines a skeleton for states - BasicState. Distinct
    states will be added in the next patches.
    
    Tested:
    - Local CI build passed
    - UTs passed
    
    Change-Id: I538beb20bae59112bb93ae7c5d2ba1817b7058af
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ virtual-media/src/events.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "interfaces/mount_point_state_machine.hpp"
+
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+#include <memory>
+#include <utility>
+
+struct BasicEvent
+{
+    explicit BasicEvent(const char* eventName) : eventName(eventName) {}
+
+    const char* eventName;
+};
+
+struct RegisterDbusEvent : public BasicEvent
+{
+    RegisterDbusEvent(
+        std::shared_ptr<sdbusplus::asio::connection> bus,
+        std::shared_ptr<sdbusplus::asio::object_server> objServer) :
+        BasicEvent(__func__), bus(std::move(bus)),
+        objServer(std::move(objServer))
+    {}
+
+    std::shared_ptr<sdbusplus::asio::connection> bus;
+    std::shared_ptr<sdbusplus::asio::object_server> objServer;
+};
+
+struct MountEvent : public BasicEvent
+{
+    explicit MountEvent(
+        std::optional<interfaces::MountPointStateMachine::Target> target) :
+        BasicEvent(__func__), target(std::move(target))
+    {}
+
+    MountEvent(const MountEvent&) = delete;
+    MountEvent(MountEvent&& other) noexcept :
+        BasicEvent(__func__), target(std::move(other.target))
+    {
+        other.target = std::nullopt;
+    }
+
+    MountEvent& operator=(const MountEvent&) = delete;
+    MountEvent& operator=(MountEvent&& other) noexcept
+    {
+        target = std::nullopt;
+        std::swap(target, other.target);
+        return *this;
+    }
+
+    ~MountEvent() = default;
+
+    std::optional<interfaces::MountPointStateMachine::Target> target;
+};
+
+struct UnmountEvent : public BasicEvent
+{
+    UnmountEvent() : BasicEvent(__func__) {}
+};
+
+struct SubprocessStoppedEvent : public BasicEvent
+{
+    SubprocessStoppedEvent() : BasicEvent(__func__) {}
+};
+
+struct UdevStateChangeEvent : public BasicEvent
+{
+    explicit UdevStateChangeEvent(const StateChange& devState) :
+        BasicEvent(__func__), devState{devState}
+    {}
+
+    StateChange devState;
+};
+
+using Event = std::variant<RegisterDbusEvent, MountEvent, UnmountEvent,
+                           SubprocessStoppedEvent, UdevStateChangeEvent>;
---
+++ virtual-media/tests/src/events_test.cpp
@@ -0,0 +1,31 @@
+#include "events.hpp"
+#include "interfaces/mount_point_state_machine.hpp"
+#include "resources.hpp"
+
+#include <gtest/gtest.h>
+
+namespace virtual_media_test
+{
+
+class EventsTest : public ::testing::Test
+{
+  protected:
+    interfaces::MountPointStateMachine::Target target{};
+    MountEvent event{std::move(target)};
+};
+
+TEST_F(EventsTest, MoveConstructorMountEventSuccesfull)
+{
+    MountEvent eventB{std::move(event)};
+    ASSERT_EQ(event.target, std::nullopt);
+    ASSERT_NE(eventB.target, std::nullopt);
+}
+
+TEST_F(EventsTest, MoveAssignmentMountEventSuccesfull)
+{
+    MountEvent eventB = std::move(event);
+    ASSERT_EQ(event.target, std::nullopt);
+    ASSERT_NE(eventB.target, std::nullopt);
+}
+
+} // namespace virtual_media_test
