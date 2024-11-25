5b93a705c447de62a79113cabe29cc34ed5fdc7d
Michal Orzel <michalx.orzel@intel.com>
2024-11-25 12:49:48 +0100

    Introduce state machine skeleton and events
    Virtual media service uses state machine built on:
    - state objects, holding state data and available transition patterns
    and,
    - events, allowing transitions between states.
    
    For each slot (defined in configuration file) independent state machine
    is created.
    
    Events structure is based on simple BasicEvent on which specific events
    like Mount, Unmount or RegisterDbusEvent are built.
    
    This commit just defines a skeleton for states - BasicState. Distinct
    states will be added in the next patches.
    
    Tested:
    - Local CI build passed
    - UTs passed
    
    Change-Id: I538beb20bae59112bb93ae7c5d2ba1817b7058af
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ virtual-media/src/interfaces/mount_point_state_machine.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "configuration.hpp"
+#include "resources.hpp"
+
+#include <boost/asio/io_context.hpp>
+
+#include <memory>
+#include <optional>
+#include <string_view>
+
+struct BasicState;
+
+namespace interfaces
+{
+
+struct MountPointStateMachine
+{
+    struct Target
+    {
+        std::unique_ptr<resource::Mount> mountPoint;
+    };
+
+    MountPointStateMachine() = default;
+
+    MountPointStateMachine(const MountPointStateMachine&) = delete;
+    MountPointStateMachine(MountPointStateMachine&&) = delete;
+
+    MountPointStateMachine& operator=(const MountPointStateMachine&) = delete;
+    MountPointStateMachine& operator=(MountPointStateMachine&&) = delete;
+
+    virtual ~MountPointStateMachine() = default;
+
+    virtual std::string_view getName() const = 0;
+    virtual Configuration::MountPoint& getConfig() = 0;
+    virtual std::optional<Target>& getTarget() = 0;
+    virtual BasicState& getState() = 0;
+    virtual int& getExitCode() = 0;
+    virtual boost::asio::io_context& getIOC() = 0;
+};
+
+} // namespace interfaces
