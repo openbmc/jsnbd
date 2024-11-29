c82611968896af8ad11bd49581e11fc03ea0bb15
Michal Orzel <michalx.orzel@intel.com>
2024-11-29 14:51:09 +0100

    Initial state - dbus API initialization
    This part adds InitialState. Used mainly to initialize dbus interfaces
    (properties and methods) for both Standard and Proxy nodes.
    
    Tested:
    DBus tree is initialized, all properties have initial content, methods
    are callable.
    > busctl tree xyz.openbmc_project.VirtualMedia
    `- /xyz
      `- /xyz/openbmc_project
        `- /xyz/openbmc_project/VirtualMedia
          |- /xyz/openbmc_project/VirtualMedia/Proxy
          | |- /xyz/openbmc_project/VirtualMedia/Proxy/Slot_0
          | `- /xyz/openbmc_project/VirtualMedia/Proxy/Slot_1
          `- /xyz/openbmc_project/VirtualMedia/Standard
            |- /xyz/openbmc_project/VirtualMedia/Standard/Slot_2
            `- /xyz/openbmc_project/VirtualMedia/Standard/Slot_3
    
    > busctl introspect xyz.openbmc_project.VirtualMedia/xyz/openbmc_pr...
    NAME TYPE SIGNATURE RESU...
    org.freedesktop.DBus.Introspectable interface - - ...
    .Introspect method - s ...
    org.freedesktop.DBus.Peer interface - - ...
    .GetMachineId method - s ...
    .Ping method - - ...
    org.freedesktop.DBus.Properties interface - - ...
    .Get method ss v ...
    .GetAll method s a{sv...
    .Set method ssv - ...
    .PropertiesChanged signal sa{sv}as - ...
    xyz.openbmc_project.VirtualMedia.MountPoint interface - - ...
    .Device property s "nbd...
    .EndpointId property s "/nb...
    .ImageURL property s "" ...
    .Socket property s "/ru...
    .Timeout property i 30 ...
    .WriteProtected property b true...
    xyz.openbmc_project.VirtualMedia.Process interface - - ...
    .Active property b false...
    .ExitCode property i -1 ...
    xyz.openbmc_project.VirtualMedia.Proxy interface - - ...
    .Mount method - b ...
    .Unmount method - b ...
    .Completion signal i - ...
    
    Local CI build an UTs have passed.
    
    Change-Id: Ic63874c6fcf8a70ff9f2a08a8f493ea0ea5c005a
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

--- virtual-media/src/interfaces/mount_point_state_machine.hpp
+++ virtual-media/src/interfaces/mount_point_state_machine.hpp
@@ -4,6 +4,8 @@
 #include "resources.hpp"
 
 #include <boost/asio/io_context.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
 
 #include <memory>
 #include <optional>
@@ -37,6 +39,17 @@ struct MountPointStateMachine
     virtual BasicState& getState() = 0;
     virtual int& getExitCode() = 0;
     virtual boost::asio::io_context& getIOC() = 0;
+
+    virtual void emitRegisterDBusEvent(
+        std::shared_ptr<sdbusplus::asio::connection> bus,
+        std::shared_ptr<sdbusplus::asio::object_server> objServer) = 0;
+
+    virtual void setMountPointInterface(
+        std::unique_ptr<sdbusplus::asio::dbus_interface>) = 0;
+    virtual void setProcessInterface(
+        std::unique_ptr<sdbusplus::asio::dbus_interface>) = 0;
+    virtual void setServiceInterface(
+        std::unique_ptr<sdbusplus::asio::dbus_interface>) = 0;
 };
 
 } // namespace interfaces
