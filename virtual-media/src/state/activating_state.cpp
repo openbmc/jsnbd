9f9c1c794be278674ae1694dc4ed7b0884c95f12
Michal Orzel <michalx.orzel@intel.com>
2024-12-10 13:51:44 +0100

    Complete set of states
    Adds states objects with main transitions between them altogether with
    uilities for USB Gadget management.
    
    Scheme for states and transitions
    
                              ┌─────────────────┐
                              │                 │
                              │  initial state  │
                              │                 │
                              └────────┬────────┘
                                       │
                                       │RegisterDbusEvent
                                       │
                          ┌────────────▼────────────┐
                          │                         │
                          │       ready state       │
                          │                         │
                          └────┬───────▲────────▲───┘
                               │       │        │
                  ┌────────────┘       │        └──────────────┐
                  │                    │             SubprocessStoppedEvent
                  │MountEvent          │SubprocessStoppedEvent │
                  │                    │                       │
      ┌───────────▼──────────┐         │                       │
      │                      ├─────────┘            ┌──────────┴───────────┐
      │   activating state   │ UdevStateChangeEvent │                      │
      │                      ├──────────────────────►  deactivating state  │
      └───────────┬──────────┘       (!insert)      │                      │
                  │                                 └──────────▲───────────┘
                  │                                            │
                  │UdevStateChangeEvent            SubprocessStoppedEvent ||
                  │      (insert)                    UdevStateChangeEvent ||
                  │                                  UnmountEvent
                  │          ┌──────────────────────┐          │
                  │          │                      │          │
                  └──────────►     active state     ├──────────┘
                             │                      │
                             └──────────────────────┘
    
    Tested:
    No real functionality added. Code compiles. Unit tests passed.
    
    Change-Id: I3163c721de90145a7cd94e3e3cb03425dd9456f4
    Signed-off-by: Michal Orzel <michalx.orzel@intel.com>
    Signed-off-by: Przemyslaw Czarnowski <przemyslaw.hawrylewicz.czarnowski@intel.com>

---
+++ virtual-media/src/state/activating_state.cpp
@@ -0,0 +1,39 @@
+#include "activating_state.hpp"
+
+#include "basic_state.hpp"
+#include "deactivating_state.hpp"
+#include "events.hpp"
+#include "interfaces/mount_point_state_machine.hpp"
+#include "ready_state.hpp"
+#include "utils/log-wrapper.hpp"
+
+#include <memory>
+#include <system_error>
+#include <utility>
+
+ActivatingState::ActivatingState(interfaces::MountPointStateMachine& machine) :
+    BasicStateT(machine)
+{}
+
+std::unique_ptr<BasicState> ActivatingState::onEnter()
+{
+    // Reset previous exit code
+    machine.getExitCode() = -1;
+
+    return nullptr;
+}
+
+std::unique_ptr<BasicState>
+    ActivatingState::handleEvent(UdevStateChangeEvent event)
+{
+    return std::make_unique<DeactivatingState>(machine, std::move(process),
+                                               std::move(gadget), event);
+}
+
+std::unique_ptr<BasicState>
+    ActivatingState::handleEvent([[maybe_unused]] SubprocessStoppedEvent event)
+{
+    LOGGER_ERROR("Process ended prematurely");
+    return std::make_unique<ReadyState>(machine, std::errc::connection_refused,
+                                        "Process ended prematurely");
+}
